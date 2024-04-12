bf539bdd1de249f4dfd17a308dd8f405961ba2db
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-04-12 23:43:10 +0530

    fixing useRef bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Counter from './features/counter/Counter'; 
+// import Counter from './features/counter/Counter'; 
+import ProductList from './features/product-list/ProductList';
 
 export default function App() {
   return (
     <div>
-      <Counter />
+      <ProductList />
     </div>
   );
 }
