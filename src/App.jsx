aa40c550179d66508da21d24031a11ea1b9da9de
ParthGupta84616 <148619447+ParthGupta84616@users.noreply.github.com>
2024-04-13 22:12:59 +0530

    Sign Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Home from './pages/Home';
-// import Counter from './features/counter/Counter'; 
-// import ProductList from './features/product-list/ProductList';
-
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/Signup';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'; // Import Switch instead of Router
 
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (<Home />),
+    },
+    {
+      path: "/login",
+      element: (<LoginPage />),
+    },
+    {
+      path: "/signup",
+      element: (<SignupPage />),
+    },
+    // {
+    //   path: "/",
+    //   element: (<Home />),
+    // },
+  ]);
   return (
-    <div>
-      <Home />
-    </div>
+          <RouterProvider router={router} />
   );
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Signup from '../features/auth/component/Signup'
+
+function SignupPage() {
+  return (
+    <div>
+        <Signup />
+
+    </div>
+  )
+}
+
+export default SignupPage
